40849f4ddf6147124b76573078db6ba4bf80afa2
RyerEduardo <eduardo.oliveira@novaandradina.org>
2019-06-24 13:02:42 -0300

    alteração no server

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const cors = require('cors')
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 
 // Iniciando o App
 const app = express();
-app.use(express.json());
-app.use(cors()) 
+app.use(bodyParser.json({limit: '10mb'}));
+//app.use(cors()) 
 
 mongoose
   .connect(
